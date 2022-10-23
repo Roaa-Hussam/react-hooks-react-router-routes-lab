1a87743c4febbb017e314601316bd911fc8ca98b
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-23 22:56:14 +0300

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((direct, index) => {
+        return (
+          <div key={index}>
+            <h2>{direct.name}</h2>
+            <ul>
+              {direct.movies.map((movie, index) => {
+                return (<li key={index} >{movie}</li>)
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Directors;
